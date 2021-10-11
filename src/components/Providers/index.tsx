e1a289c116896890fe12d3cdbd4d94fee5ee757e
Daniel Norman <daniel@2color.me>
2021-10-12 01:37:28 +0200

    Add session provider

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react'
 import SEO from './SEO'
 import Toast from './Toaster'
+import { SessionProvider } from 'next-auth/react'
+import { Session } from 'next-auth'
 
 interface Props {
   children?: any
+  session: Session
 }
 
-export default function Providers({ children }: Props) {
+export default function Providers({ session, children }: Props) {
   return (
     <>
       <SEO />
       <Toast />
-      {children}
+      <SessionProvider session={session}>{children}</SessionProvider>
     </>
   )
 }
