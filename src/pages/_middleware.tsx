985922b2d80f7f0a16d89f1b49eaa28bc0f51fd9
Daniel Norman <daniel@2color.me>
2021-10-15 17:02:05 +0200

    Try without crypto api

--- src/pages/_middleware.tsx
+++ src/pages/_middleware.tsx
@@ -9,30 +9,30 @@ export async function middleware(evt: NextFetchEvent) {
   if (pathname !== '/') {
     return NextResponse.next()
   }
-  const ipHash = await sha256(evt.request.ip)
+  // const ipHash = await sha256(evt.request.ip)
 
   await prisma.visitor.upsert({
     where: {
-      ipHash: ipHash,
+      ipHash: evt.request.ip,
     },
     create: {
-      ipHash: ipHash,
+      ipHash: evt.request.ip,
       lastSeen: new Date(),
     },
     update: {
-      ipHash: ipHash,
+      ipHash: evt.request.ip,
       lastSeen: new Date(),
     },
   })
   return NextResponse.next()
 }
 
-async function sha256(str: string): Promise<string> {
-  const buf = await crypto.subtle.digest(
-    'SHA-256',
-    new TextEncoder().encode(str)
-  )
-  return Array.prototype.map
-    .call(new Uint8Array(buf), (x) => ('00' + x.toString(16)).slice(-2))
-    .join('')
-}
+// async function sha256(str: string): Promise<string> {
+//   const buf = await crypto.subtle.digest(
+//     'SHA-256',
+//     new TextEncoder().encode(str)
+//   )
+//   return Array.prototype.map
+//     .call(new Uint8Array(buf), (x) => ('00' + x.toString(16)).slice(-2))
+//     .join('')
+// }
