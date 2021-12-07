b2706d3aead6eb4987ec2c412c4a3e6e0ec1dd7c
Daniel Norman <daniel@2color.me>
2021-12-07 11:44:31 +0100

    Disable middleware

--- next.config.js
+++ next.config.js
@@ -4,17 +4,8 @@ module.exports = {
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET,
   },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.alias['@prisma/client$'] = require.resolve('@prisma/client')
-    return config
-  },
-  async redirects() {
-    return [
-      // {
-      //   source: '/uses',
-      //   destination: '/stack',
-      //   permanent: true,
-      // },
-    ]
-  },
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   config.resolve.alias['@prisma/client$'] = require.resolve('@prisma/client')
+  //   return config
+  // },
 }
